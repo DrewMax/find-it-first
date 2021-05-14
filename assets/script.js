4432445f061cdd3e1c885bbce6046107d74b9b88
Drew Maxwell <ajmaxwell.reviresco@gmail.com>
2021-05-14 12:35:56 +0100

    products and shopping cart

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,78 @@
+if (document.readyState == 'loading') {
+    document.addEventListener("DOMContentloading", ready)
+} else {
+    ready()
+}
+
+function ready() {
+var remove = document.querySelector("btn-danger");
+    for (var i = 0; i < remove.length; i++) {
+        var button = remove[i]
+        button.addEventListener('click', function(event) {
+            var buttonClicked = event.target;
+            buttonClicked.parentElement.parentElement.remove()
+        })
+    }
+    var quantityInputs = document.querySelector(".cart-quantity-input");
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged)
+    }
+}
+
+var updateCartTotal = function() {
+    var cartItemContainer = document.querySelector(".cart-items");
+    var cartRows = cartItemContainer.querySelector(".cart-row");
+    var total = 0;
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i];
+        var priceElement = cartRow.querySelector(".cart-price")[0]
+        var quantityElement = cartRow.querySelector(".cart-quantity-input")
+        var price = parseFloat(priceElement.innerText.replace('£', ''))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+    }
+    document.querySelector("cart-total-price")[0].innerText = '£' + total;
+}
+
+var quantityChanged = function(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    updateCartTotal();
+}
+
+var addToCartButtons = document.querySelector(".add-button")
+    for (var i = 0; i < addToCartButtons; i++) {
+        var button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
+    }
+
+var addToCartClicked = function(event) {
+    var button = event.target
+    var shopItem = parentElement.parentElement
+    var title = shopItem.querySelector(".title")[0].innerText
+    var price = shopItem.querySelector(".price")[0].innerText
+    var imageSrc = shopItem.querySelector(".image").src
+    addItemToCart(title, price, imageSrc);
+    updateCartTotal()
+}
+var addItemToCart = function(title, price, imageSrc) {
+    var cartRow = 
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 // modal is triggered
 $("#task-form-modal").on("show.bs.modal", function() {
